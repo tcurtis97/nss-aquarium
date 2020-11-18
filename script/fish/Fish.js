d7047c4d832086771b1cee732b45af23ecac1ca9
tcurtis97 <timcurtis97@yahoo.com>
2020-11-18 10:35:11 -0600

    add js for fish and made files for tips

---
+++ script/fish/Fish.js
@@ -0,0 +1,15 @@
+
+
+
+export const Fish = (fish) => {
+    return `
+        <section class="fish card">
+            <div><img  class="fish__image image--card" src="images/${fish.image}" /></div>
+            <div class="fish__name">${fish.name}</div>
+            <div class="fish__species">${fish.species}</div>
+            <div class="fish__length">${fish.length}</div>
+            <div class="fish__location">${fish.location}</div>
+            <div class="fish__diet">${fish.food}</div>
+        </section>
+    `
+  }
